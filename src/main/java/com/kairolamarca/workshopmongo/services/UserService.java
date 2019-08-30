3dad9726dfcc449b47d99ab46d9963165f68dcf5
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2019-08-30 19:51:05 -0300

    Obtendo um usuário por ID e criando uma classe de exception padrão
    Obtendo um usuário por ID e criando uma classe de exception padrão

--- src/main/java/com/kairolamarca/workshopmongo/services/UserService.java
+++ src/main/java/com/kairolamarca/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.kairolamarca.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kairolamarca.workshopmongo.domain.User;
 import com.kairolamarca.workshopmongo.repository.UserRepository;
+import com.kairolamarca.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,17 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		/*User user = repo.findOne(id);
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}*/
+		
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
 	}
 	
 }
