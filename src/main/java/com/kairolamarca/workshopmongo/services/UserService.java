a3d85764c7c2c7679a15028159ce63c079ef01f4
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2019-08-30 20:13:18 -0300

    inserção de usuário com POST
    inserção de usuário com POST

--- src/main/java/com/kairolamarca/workshopmongo/services/UserService.java
+++ src/main/java/com/kairolamarca/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kairolamarca.workshopmongo.domain.User;
+import com.kairolamarca.workshopmongo.dto.UserDTO;
 import com.kairolamarca.workshopmongo.repository.UserRepository;
 import com.kairolamarca.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -29,6 +30,14 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
 }
